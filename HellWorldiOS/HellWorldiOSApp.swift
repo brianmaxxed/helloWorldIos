d9da9098aafb5c98244d3cd7068846a580bb5dd0
Brian Lewis <brianismaxxed@gmail.com>
2022-01-27 01:46:55 -0800

    Initial Commit

---
+++ HellWorldiOS/HellWorldiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HellWorldiOSApp.swift
+//  HellWorldiOS
+//
+//  Created by Brian Maxx on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct HellWorldiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
